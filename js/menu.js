b9bee80a34225e240a616b44780e2ab142567a2b
entreunosyceros <admin@entreunosyceros.net>
2022-12-27 18:59:00 +0100

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+let btnHamburguesa = document.getElementById("btn-hamburguesa");
+let menuNavegacion = document.getElementById("menu-navegacion");
+btnHamburguesa.addEventListener('click', function(){
+    menuNavegacion.classList.toggle("viewMenu");
+});
+
+$(".angle-view-sub-menu").click(function(){
+  $(this).siblings("ul").toggle();
+})
